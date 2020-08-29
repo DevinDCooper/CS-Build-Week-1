d96b0b87af35b55c30f6ca6e58aaf833e19f7204
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-08-28 19:19:53 -0700

    Updates

---
+++ precache-manifest.180c18c5a017072911161b6ee22bc285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b293c8d4f76005073542d348ae203879",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "67d24539bbac88ab4305",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.74e82d11.chunk.css"
+  },
+  {
+    "revision": "ca4d08c8c30fc96717a7",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.038d2dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.038d2dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d24539bbac88ab4305",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.31a8c706.chunk.js"
+  },
+  {
+    "revision": "abefc89a4af264e128f6",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.9073b8c7.js"
+  },
+  {
+    "revision": "e79c43cff8e12deb8b6a89792ee15681",
+    "url": "/%3Cgithub-repo-name%3E/static/media/Space-Invaders.e79c43cf.jpg"
+  }
+]);
